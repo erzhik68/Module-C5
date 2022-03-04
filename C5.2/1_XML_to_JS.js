d3b9933ffcdd6adb40d2ec2f337eb8eb625d127f
erzhik68 <93429091+erzhik68@users.noreply.github.com>
2022-03-04 17:24:36 +0500

    Add files via upload

--- C5.2/1_XML_to_JS.js
+++ C5.2/1_XML_to_JS.js
@@ -1,1 +1,57 @@
+﻿/* подготовка данных */
 
+/* Создание экземпляра класса DOMParser. он позволит нам парсить XML */
+const parser = new DOMParser();
+//console.log('parser', parser);
+// XML, который будем парсить
+const xmlString = `
+  <list>
+	<student>
+		<name lang="en">
+			<first>Ivan</first>
+			<second>Ivanov</second>
+		</name>
+		<age>35</age>
+		<prof>teacher</prof>
+	</student>
+	<student>
+		<name lang="ru">
+			<first>Петр</first>
+			<second>Петров</second>
+		</name>
+		<age>58</age>
+		<prof>driver</prof>
+	</student>
+  </list>
+`;
+// console.log("xmlString", xmlString);
+
+/* получение данных */
+// Парсинг XML
+const xmlDOM = parser.parseFromString(xmlString, "text/xml");
+
+// получение всех DOM-нод
+const listNode = xmlDOM.querySelector("list");
+const studentNode = listNode.querySelectorAll("student");
+// console.log(studentNode.length);
+// console.log(studentNode);
+let arrayFromStudents = [];
+
+for (let i = 0; i < studentNode.length; i++) {
+	let student_object = studentNode[i];
+	let nameNode = student_object.querySelector("name");
+	let firstNode = nameNode.querySelector("first");
+	let secondNode = nameNode.querySelector("second");
+	let ageNode = student_object.querySelector("age");
+	let profNode = student_object.querySelector("prof");
+	let langAttr = nameNode.getAttribute("lang");
+//  console.log(firstNode.textContent, secondNode.textContent, ageNode.textContent, profNode.textContent, langAttr)
+	arrayFromStudents[i] = {
+		name: firstNode.textContent + ' ' + secondNode.textContent,
+		age: Number(ageNode.textContent),
+		prof: profNode.textContent,
+		lang: langAttr}
+}
+const result = {'list': arrayFromStudents}
+
+console.log(result)
---
+++ C5.2/2_JSON_to_JS.js
@@ -0,0 +1,33 @@
+﻿const parser = new DOMParser();
+
+const jsonString = `{
+ "list": [
+  {
+   "name": "Petr",
+   "age": "20",
+   "prof": "mechanic"
+  },
+  {
+   "name": "Vova",
+   "age": "60",
+   "prof": "pilot"
+  }
+ ]
+}`;
+
+const jsonObject = JSON.parse(jsonString);
+
+let students = [];
+
+let i;
+for (i = 0; i < jsonObject.list.length; i++) {
+  let student_let = jsonObject.list[i];
+  students[i] = {
+    name: student_let.name,
+    age: Number(student_let.age),
+    prof: student_let.prof
+  };
+}
+
+let resutls = { list: students };
+console.log(resutls);
